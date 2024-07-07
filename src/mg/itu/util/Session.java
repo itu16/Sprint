c6f3c4c3cbd57ef6d51a650cba24187b09bb4060
itu16 <ramaherisonihorenantsoa@gmail.com>
2024-07-07 15:11:02 +0300

    Session sprint 8

---
+++ src/mg/itu/util/Session.java
@@ -0,0 +1,31 @@
+package mg.itu.util;
+
+import jakarta.servlet.http.HttpSession;
+
+public class Session {
+    private final HttpSession httpSession;
+
+    public Session(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
+    public void setAttribute(String key, Object value) {
+        httpSession.setAttribute(key, value);
+    }
+
+    public Object getAttribute(String key) {
+        return httpSession.getAttribute(key);
+    }
+
+    public void removeAttribute(String key) {
+        httpSession.removeAttribute(key);
+    }
+
+    public void invalidate() {
+        httpSession.invalidate();
+    }
+
+    public String getId() {
+        return httpSession.getId();
+    }
+}
